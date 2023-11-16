d1e636eed5d9681f41840e0a9e5a67012ed77954
Kostiantyn Bulgaka <kostyabulgaka3@gmail.com>
2023-11-16 15:50:45 +0200

    Setting up Entity Framework code-first database

---
+++ UserQuizApp.Data/Quiz.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserQuizApp.Data
+{
+    public class Quiz
+    {
+        public int Id { get; set; }
+        public string QuizName { get; set; }
+
+        public List<Question> Questions { get; set; }
+
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
+}
