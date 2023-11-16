5aab9bda122919a273f226e7e99a491dcefceba3
Kostiantyn Bulgaka <kostyabulgaka3@gmail.com>
2023-11-16 18:46:01 +0200

    Add login API functionality

---
+++ UserQuizApp/Controllers/LoginController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using UserQuizApp.Data;
+
+namespace UserQuizApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        private IConfiguration _config;
+
+        public LoginController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpPost("Login")]
+        public IActionResult Login(string name, string password)
+        {
+            var isUser = IsAuthorizedUser(name, password);
+
+            if (isUser)
+            {
+                var token = Generate(name, password);
+                return Ok(token);
+            }
+            return NotFound();
+
+        }
+
+        private string Generate(string name, string password)
+        {
+            var securityKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, name)
+            };
+
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"],
+             _config["Jwt:Audience"],
+             claims,
+             expires: DateTime.Now.AddMinutes(15),
+             signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+
+        private bool IsAuthorizedUser(string username, string password)
+        {
+            using (var context = new QuizDataContext())
+            {
+                if (context.Users.Where(x => x.Name.Equals(username) && x.Password.Equals(password)).Single() != null)
+                {
+                    return true;
+                }
+                return false;
+            }
+        }
+    }
+}
